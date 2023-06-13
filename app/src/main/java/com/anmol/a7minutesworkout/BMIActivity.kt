331f366b8e4bfd439fb91e4d516eca86c5ad396e
Anmol Kumar <1RN20IS020@RNSIT.onmicrosoft.com>
2023-06-14 01:50:38 +0530

    Adding radioView with Custom RadioButtons and US view

--- app/src/main/java/com/anmol/a7minutesworkout/BMIActivity.kt
+++ app/src/main/java/com/anmol/a7minutesworkout/BMIActivity.kt
@@ -11,6 +11,17 @@ import java.math.RoundingMode
 //Creating a BMI calculator activity.
 class BMIActivity : AppCompatActivity() {
 
+    // Added variables for METRIC and US UNITS views and a variable for displaying the current selected view.
+    // START
+    companion object {
+        private const val METRIC_UNITS_VIEW = "METRIC_UNIT_VIEW" // Metric Unit View
+        private const val US_UNITS_VIEW = "US_UNIT_VIEW" // US Unit View
+    }
+
+    private var currentVisibleView: String =
+        METRIC_UNITS_VIEW // A variable to hold a value to make a selected view visible
+    // END
+
     //create binding for the activity
     private var binding: ActivityBmiBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,18 +44,42 @@ class BMIActivity : AppCompatActivity() {
         }
         // END
 
-        // Adding a click event to METRIC UNIT Calculate button and after valid input calculating it.
+        // When the activity is launched make METRIC UNITS VIEW visible.
         // START
+        makeVisibleMetricUnitsView()
+        // END
+        // Adding a check change listener to the radio group and according to the radio button.
+        // START
+        // Radio Group change listener is set to the radio group which is added in XML.
+        //we use _ for the first value because we don't need it
+        binding?.rgUnits?.setOnCheckedChangeListener { _, checkedId: Int ->
+
+            // Here if the checkId is METRIC UNITS view then make the view visible else US UNITS view.
+            if (checkedId == R.id.rbMetricUnits) {
+                makeVisibleMetricUnitsView()
+            } else {
+                makeVisibleUsUnitsView()
+            }
+        }
+        // END
         // Button will calculate the input values in Metric Units
         binding?.btnCalculateUnits?.setOnClickListener {
+            calculateUnits()
+        }
 
+    }
+
+    private fun calculateUnits(){
+        // Handling the current visible view and calculating US UNITS view input values if they are valid.
+        // START
+        if (currentVisibleView == METRIC_UNITS_VIEW) {
             // The values are validated.
             if (validateMetricUnits()) {
 
-                // The height value is converted to a float value and divided by 100 to convert it to meter.
+                // The height value is converted to float value and divided by 100 to convert it to meter.
                 val heightValue: Float = binding?.etMetricUnitHeight?.text.toString().toFloat() / 100
 
-                // The weight value is converted to a float value
+                // The weight value is converted to float value
                 val weightValue: Float = binding?.etMetricUnitWeight?.text.toString().toFloat()
 
                 // BMI value is calculated in METRIC UNITS using the height and weight value.
@@ -52,11 +87,97 @@ class BMIActivity : AppCompatActivity() {
 
                 displayBMIResult(bmi)
             } else {
-                Toast.makeText(this@BMIActivity, "Please enter valid values.", Toast.LENGTH_SHORT)
+                Toast.makeText(
+                    this@BMIActivity,
+                    "Please enter valid values.",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+        } else {
+
+            // The values are validated.
+            if (validateUsUnits()) {
+
+                val usUnitHeightValueFeet: String =
+                    binding?.etUsMetricUnitHeightFeet?.text.toString() // Height Feet value entered in EditText component.
+                val usUnitHeightValueInch: String =
+                    binding?.etUsMetricUnitHeightInch?.text.toString() // Height Inch value entered in EditText component.
+                val usUnitWeightValue: Float = binding?.etUsMetricUnitWeight?.text.toString()
+                    .toFloat() // Weight value entered in EditText component.
+
+                // Here the Height Feet and Inch values are merged and multiplied by 12 for converting it to inches.
+                val heightValue =
+                    usUnitHeightValueInch.toFloat() + usUnitHeightValueFeet.toFloat() * 12
+
+                // This is the Formula for US UNITS result.
+                // Reference Link : https://www.cdc.gov/healthyweight/assessing/bmi/childrens_bmi/childrens_bmi_formula.html
+                val bmi = 703 * (usUnitWeightValue / (heightValue * heightValue))
+
+                displayBMIResult(bmi) // Displaying the result into UI
+            } else {
+                Toast.makeText(
+                    this@BMIActivity,
+                    "Please enter valid values.",
+                    Toast.LENGTH_SHORT
+                )
                     .show()
             }
         }
-        // END
+    }
+    // Function is used to validate the input values for US UNITS.
+    private fun validateUsUnits(): Boolean {
+        var isValid = true
+
+        when {
+            binding?.etUsMetricUnitWeight?.text.toString().isEmpty() -> {
+                isValid = false
+            }
+            binding?.etUsMetricUnitHeightFeet?.text.toString().isEmpty() -> {
+                isValid = false
+            }
+            binding?.etUsMetricUnitHeightInch?.text.toString().isEmpty() -> {
+                isValid = false
+            }
+        }
+
+        return isValid
+    }
+
+    // Making a function to make the METRIC UNITS view visible.
+    // START
+    // Function is used to make the METRIC UNITS VIEW visible and hide the US UNITS VIEW.
+
+    private fun makeVisibleMetricUnitsView() {
+        currentVisibleView = METRIC_UNITS_VIEW // Current View is updated here.
+        binding?.tilMetricUnitWeight?.visibility = View.VISIBLE // METRIC  Height UNITS VIEW is Visible
+        binding?.tilMetricUnitHeight?.visibility = View.VISIBLE // METRIC  Weight UNITS VIEW is Visible
+        binding?.tilUsMetricUnitWeight?.visibility = View.GONE // make weight view Gone.
+        binding?.tilMetricUsUnitHeightFeet?.visibility = View.GONE // make height feet view Gone.
+        binding?.tilMetricUsUnitHeightInch?.visibility = View.GONE // make height inch view Gone.
+
+        binding?.etMetricUnitHeight?.text!!.clear() // height value is cleared if it is added.
+        binding?.etMetricUnitWeight?.text!!.clear() // weight value is cleared if it is added.
+
+        binding?.llDiplayBMIResult?.visibility = View.INVISIBLE
+    }
+    // END
+
+    // Making a function to make the US UNITS view visible.
+    // START
+    private fun makeVisibleUsUnitsView() {
+        currentVisibleView = US_UNITS_VIEW // Current View is updated here.
+        binding?.tilMetricUnitHeight?.visibility = View.INVISIBLE // METRIC  Height UNITS VIEW is InVisible
+        binding?.tilMetricUnitWeight?.visibility = View.INVISIBLE // METRIC  Weight UNITS VIEW is InVisible
+        binding?.tilUsMetricUnitWeight?.visibility = View.VISIBLE // make weight view visible.
+        binding?.tilMetricUsUnitHeightFeet?.visibility = View.VISIBLE // make height feet view visible.
+        binding?.tilMetricUsUnitHeightInch?.visibility = View.VISIBLE // make height inch view visible.
+
+        binding?.etUsMetricUnitWeight?.text!!.clear() // weight value is cleared.
+        binding?.etUsMetricUnitHeightFeet?.text!!.clear() // height feet value is cleared.
+        binding?.etUsMetricUnitHeightInch?.text!!.clear() // height inch is cleared.
+
+        binding?.llDiplayBMIResult?.visibility = View.INVISIBLE
     }
 
 
